8f9e87679f744b8d828bd7c423670b4c9c297281
fraoulmas <fraoulmas@gmail.com>
2022-06-10 00:52:32 +0300

    delete and sqare matrix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for i in list(a_dictionary):
-        if a_dictionary[i] == value:
-            del a_dictionary[i]
-        return a_dictionary
-
+    for k, v in list(a_dictionary.items()):
+        if v is value:
+            a_dictionary.pop(k)
+    return a_dictionary
