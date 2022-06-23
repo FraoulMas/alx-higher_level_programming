060ecea4951d2c035aea7ef3db475d2ac07bc7ea
fraoulmas <fraoulmas@gmail.com>
2022-06-23 14:00:32 +0300

    removed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_integer_err(value):
-    import sys
-    try:
-        print("{:d}".format(value))
-        return True
-    except Exception as exe:
-        print("Exception: {}".format(exe), file=sys.stderr)
-        return False
